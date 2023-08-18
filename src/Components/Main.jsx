e3babd5d97db59a5d09cd0470d700901233d6c05
thizzy824 <111156467+thizzy824@users.noreply.github.com>
2023-08-17 21:08:36 -0700

    Components framework

---
+++ src/Components/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+ const Main = () => {
+    return(
+        <>
+        <div className="container">
+            <div className="left-content">
+
+            </div>
+            <div className="right-content">
+
+            </div>
+        </div>
+        </>
+    )
+ }
+
+ export default Main
---
+++ src/Components/Pokeinfo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Pokeinfo() {
+    return (
+        <div>
+            <h1>Articuno</h1>
+        </div>
+    );
+}
+
+export default Pokeinfo;
